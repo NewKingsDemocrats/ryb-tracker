ee5701ccb507858dd62f0d613783cc977ff8082d
Isaac Anderson <kavunshiva@gmail.com>
2019-07-29 22:23:11 -0400

    standardize and validate addresses, fetch AD and ED from CC Sunlight API

--- query_sheet.rb
+++ query_sheet.rb
@@ -4,13 +4,24 @@ require 'set'
 require 'pry'
 require 'googleauth'
 require 'google/apis/sheets_v4'
+require 'google/apis/drive_v3'
 
 ENV_VARS_SPREADSHEET_ID = '10yQzyt4_JMZmFeVYLaWi2DinP-oPiq6cfwXfSFsoEvw'
 DISTRICT_TO_SPREADSHEET_ID = 'district to spreadsheet ID'
 MASTER_SCHEMA_SPREADSHEET_ID = '1KABFR083wl6Ok0WEIsPs1lefZt7U9PJz1iuneQ7Prc0'
 MASTER_SCHEMA_SHEET_ID = 'Candidate View'
 
-def validate_spreadsheets_columns
+def update_district_spreadsheets
+  begin
+    if spreadsheets_columns_valid?
+      binding.pry
+    end
+  rescue => e
+    puts e
+  end
+end
+
+def spreadsheets_columns_valid?
   if invalid_speadsheets_columns && invalid_speadsheets_columns.length > 0
     error_message = "The following spreadsheets had errors:\n"
     invalid_speadsheets_columns.each do |invalid_spreadsheet|
@@ -41,6 +52,16 @@ end
 def service
   @service ||= begin
     s = Google::Apis::SheetsV4::SheetsService.new
+    s.authorization = authorize_service
+    s
+  rescue
+    raise 'Could not authorize service.'
+  end
+end
+
+def drive_service
+  @service ||= begin
+    s = Google::Apis::DriveV3::DriveService.new
     s.authorization = authorize_service
     s
   rescue
@@ -89,10 +110,6 @@ def sheet_columns(spreadsheet_id, page_id)
 end
 
 def invalid_speadsheets_columns
-  assembly_district_sheets = read_sheet(
-    ENV_VARS_SPREADSHEET_ID,
-    DISTRICT_TO_SPREADSHEET_ID,
-  )
   master_schema_columns = sheet_columns(
     MASTER_SCHEMA_SPREADSHEET_ID,
     MASTER_SCHEMA_SHEET_ID,
@@ -118,6 +135,11 @@ def invalid_speadsheets_columns
     end
     arr
   end
+end
+
+def assembly_district_sheets
+  @assembly_district_sheets ||=
+    read_sheet(ENV_VARS_SPREADSHEET_ID, DISTRICT_TO_SPREADSHEET_ID)
 end
 
 def missing_columns(schema_columns, ad_sheet_columns)
@@ -150,6 +172,118 @@ def add_errors_to_message(
   error_message += ad_message + "\n"
 end
 
-validate_spreadsheets_columns
+def candidates_by_attribute(candidates, attribute, one_to_many=false)
+  candidates.select do |candidate|
+    candidate[attribute] &&
+      candidate[attribute].length > 0
+  end.reduce({}) do |obj, candidate|
+    if obj[candidate[attribute]]
+      obj[candidate[attribute]] << candidate
+    else
+      obj[candidate[attribute]] = one_to_many ? [candidate] : candidate
+    end
+    obj
+  end
+end
 
+def find_diffs(imported_candidates)
+  imported_candidates_by_nationbuilder_id = candidates_by_attribute(
+    imported_candidates,
+    'nationbuilder_id',
+  )
+  sheets_data = assembly_district_sheets.reduce({}) do |obj, ad_sheet|
+    obj[ad_sheet['assembly_district']] = candidates_by_attribute(
+      read_sheet(ad_sheet['spreadsheet_id'], 'Candidate View'),
+      'Email',
+    )
+    obj
+  end
+  binding.pry
+end
+
+def column_to_letter(num)
+  char = (num % 26 + 65).chr
+  remainder = num/26
+  if num >= 26
+    column_to_letter(remainder - 1) + char
+  else
+    char
+  end
+end
+
+def get_ad_and_ed_from_cc_sunlight(address)
+  base_uri = 'https://ccsunlight.org/api/v1/address/'
+  uri = URI(base_uri + CGI.escape(address))
+  request = Net::HTTP::Get.new(path=uri)
+  response = Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
+    http.request(request)
+  end
+  ad, ed = JSON.parse(response.body).values_at('ad', 'ed')
+  {
+    ad: ad,
+    ed: ed,
+  }
+end
+
+def format_address(candidate)
+  unless candidate['primary_address1'] &&
+    candidate['primary_address1'].length > 0 &&
+    candidate['primary_city'] &&
+    candidate['primary_city'].length > 0 &&
+    candidate['primary_city'].match(/brooklyn|new york/i) &&
+    candidate['primary_state'] &&
+    candidate['primary_state'].length > 0 &&
+    candidate['primary_state'].match(/ny/i) &&
+    candidate['primary_zip'] &&
+    candidate['primary_zip'].length > 0 &&
+    candidate['primary_zip'].length == 5
+      return nil
+  end
+  "#{
+    titleize(candidate['primary_address1'])
+  }#{
+    if candidate['primary_address2'] && candidate['primary_address2'].length > 0
+      ' ' + candidate['primary_address2']
+    else
+      ''
+    end
+  }, #{
+    titleize(candidate['primary_city'])
+  }, #{
+    candidate['primary_state'].upcase
+  } #{
+    candidate['primary_zip']
+  }"
+end
+
+def titleize(str)
+  str
+    .to_s.split(/ |\_|\-/)
+    .map(&:capitalize)
+    .join(' ')
+end
+
+def map_address_ad_and_ed_to_candidates(candidates)
+  candidates.map do |candidate|
+    candidate['address'] = format_address(candidate)
+    if candidate['address']
+      puts candidate['address']
+      candidate['ad'], candidate['ed'] =
+        get_ad_and_ed_from_cc_sunlight(candidate['address'])
+          .values_at(:ad, :ed)
+    end
+    candidate
+  end
+end
+
+# spreadsheets_columns_valid?
+
+# find_diffs(
+#   read_sheet(
+#     '1Jl_Gr-WcRstFhHNHGOVin9IAxfuCaXhNDnOAqOfHY8s',
+#     'nationbuilder-people-export-2019-07-09-2131',
+#   )
+# )
 # puts read_sheet(ARGV[0], ARGV[1])
+
+binding.pry
